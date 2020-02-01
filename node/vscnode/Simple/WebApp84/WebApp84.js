89b38030908ed4e12db020ccfa613d38b0ad84c5
AppaBaba <darleg@outlook.com>
2020-02-01 15:54:19 -0800

    Update WebApp84.js

--- node/vscnode/Simple/WebApp84/WebApp84.js
+++ node/vscnode/Simple/WebApp84/WebApp84.js
@@ -10,18 +10,25 @@ console.log('WebApp84 running at http://127.0.0.1:8084/');
 function OnReq(req, res){
     var pn = url.parse(req.url).pathname;
     console.log('pathname ' + pn);
-    displayPage(res, pn);
+    PageDisp(res, pn);
 }
 
-function displayPage(res, pn){
+function PageDisp(res, pn){
+  if(PageMap[pn]){
     // http header with OK message(200)
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(PageMap['/']); 
+    res.write(PageMap[pn]); 
     res.write('Appa says Hi There!'); //response to the client
     res.end(); //end response
+  }else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write('404 Page Not Found');
+    res.end();
+  }
 }
 
 var PageMap = {
   '/' : '<h1>AppaBaba Home Page</h1>',
-  '/me' : '<h1>It me again AppaBaba</h1>'
+  '/me' : '<h1>It me again AppaBaba</h1>',
+  '/summary' : '<h1>Sumary Page</h1>'
 }
