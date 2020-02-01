d14b71c5982ec0138de6e23168356d4783f34b12
AppaBaba <darleg@outlook.com>
2020-02-01 13:39:53 -0800

    back septed to webapp83

--- node/vscnode/Simple/WebApp83/WebApp83.js
+++ node/vscnode/Simple/WebApp83/WebApp83.js
@@ -3,7 +3,7 @@ var http = require('http'); // include http module
 var url = require('url');   // include url module
  
 // create a server
-http.createServer(OnReq).listen(8083); //WebApp83 listens on port 8083
+http.createServer(OnReq).listen(8083); //WebApp84 listens on port 8083
 // print message to console
 console.log('WebApp83 running at http://127.0.0.1:8083/');
 
@@ -15,7 +15,8 @@ function OnReq(req, res){
 
 function displayPage(res, pn){
     // http header with OK message(200)
-    res.writeHead(200, {'Content-Type': 'text/html'});  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(PageMap['/']); 
     res.write('Appa says Hi There!'); //response to the client
     res.end(); //end response
 }
