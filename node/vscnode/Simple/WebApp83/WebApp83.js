df5fd3962be0054752558c9d7d084d17902ed04e
AppaBaba <darleg@outlook.com>
2020-02-01 14:48:17 -0800

    Update WebApp83.js

--- node/vscnode/Simple/WebApp83/WebApp83.js
+++ node/vscnode/Simple/WebApp83/WebApp83.js
@@ -3,7 +3,7 @@ var http = require('http'); // include http module
 var url = require('url');   // include url module
  
 // create a server
-http.createServer(OnReq).listen(8083); //WebApp84 listens on port 8083
+http.createServer(OnReq).listen(8083); //WebApp83 listens on port 8083
 // print message to console
 console.log('WebApp83 running at http://127.0.0.1:8083/');
 
