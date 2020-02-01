a34208b18c7c8c387533a198c6ca29f9bfca903d
AppaBaba <darleg@outlook.com>
2020-01-31 23:48:55 -0800

    add function to WebApp82

--- node/vscnode/Simple/WebApp82/WebApp82.js
+++ node/vscnode/Simple/WebApp82/WebApp82.js
@@ -2,12 +2,15 @@
 var http = require('http'); 
  
 // create a server
-http.createServer(function (req, res) { 
+http.createServer(OnReq).listen(8082); //WebApp81 listens on port 8081
+// print message to console
+console.log('WebApp82 running at http://127.0.0.1:8082/');
+
+function OnReq(req, res){
     // http header with OK message(200)
     res.writeHead(200, {'Content-Type': 'text/html'});  
     res.write('Appa says Hi There!'); //response to the client
     res.end(); //end response
-}).listen(8081); //webapp81 listens on port 8081
+}
 
-// print message to console
-console.log('WebApp81 running at http://127.0.0.1:8081/');
+
