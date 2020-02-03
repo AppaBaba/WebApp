70fbc6600af702bcdfe6f819fe9aab439cdd7a69
AppaBaba <darleg@outlook.com>
2020-02-03 11:56:11 -0800

    update pagedisp

--- node/vscnode/Simple/WebApp85/disp.js
+++ node/vscnode/Simple/WebApp85/disp.js
@@ -1,10 +1,10 @@
-var page = require('./page');
+var page = require('./disp');
 
 function PageDisp(res, pn){
-  if(page.PageMap[pn]){
+  if(disp.PageMap[pn]){
     // http header with OK message(200)
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(page.PageMap[pn]); 
+    res.write(disp.PageMap[pn]); 
     res.write('Appa says Hi There!'); //response to the client
     res.end(); //end response
   }else {
