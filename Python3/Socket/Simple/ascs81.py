674addf29f2c423c2e45c5f1c583603fc11a6d4f
AppaBaba <darleg@outlook.com>
2020-02-04 11:46:46 -0800

    add document

--- Python3/Socket/Simple/ascs81.py
+++ Python3/Socket/Simple/ascs81.py
@@ -1,9 +1,19 @@
 # Python 3 A Simple Client Socket
 from socket import *
+
+# create socket object
 cs = socket(AF_INET, SOCK_STREAM)
+
+# get localhost name
 host = gethostname()                           
 port = 8881
-cs.connect((host, port))                               
-msg = cs.recv(1024)                                     
+
+# connetion to host on the port
+cs.connect((host, port))
+
+# no more than 1024 bytes
+msg = cs.recv(1024)
+
+# close client
 cs.close()
 print (msg.decode('ascii'))
--- Python3/Socket/Simple/asss81.py
+++ Python3/Socket/Simple/asss81.py
@@ -1,14 +1,26 @@
 # Python 3 A Simple Server Socket
-from socket import *                                      
-ss = socket(AF_INET, SOCK_STREAM) 
+from socket import *
+
+# create socket object
+ss = socket(AF_INET, SOCK_STREAM)
+
+# localhost
 host = gethostname()                           
-port = 8881                                          
-ss.bind((host, port))                                  
-ss.listen(3)                                           
+port = 8881
+
+# bind to the port
+ss.bind((host, port))
+
+# up to 3 requests
+ss.listen(3)
+
 while True:
    # establish a connection
-   cs,addr = ss.accept()      
+   cs,addr = ss.accept()
+   
    print("connection from %s" % str(addr))
+
+   # send mesage to client
    msg = 'Hi there' + "\r\n"
    cs.send(msg.encode('ascii'))
    cs.close()
